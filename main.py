3023c62438dfc0c0a458231e5507807b0c9a8f82
feraska <45358056+feraska@users.noreply.github.com>
2022-08-08 20:10:36 +0300

    update

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 import os
-
+import sys
 import face_recognition
 import cv2
 import numpy as np
 
 video_capture = cv2.VideoCapture(0)
 
-
 path_dir = "images/"
 
 known_face_encodings = []
 known_face_names = []
-for name in os.listdir(path_dir):
-    image = face_recognition.load_image_file(path_dir+"/"+name)
-    face_encoding = face_recognition.face_encodings(image)[0]
-    known_face_encodings.append(face_encoding)
-    known_face_names.append(name[0:name.index(".")])
 
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
 
-    face_locations = []
-    face_encodings = []
-    face_names = []
-    process_this_frame = True
+try:
+    for name in os.listdir(path_dir):
+        image = face_recognition.load_image_file(path_dir+ name)
+        face_encoding = face_recognition.face_encodings(image)
+        if len(face_encoding) > 0:
+            face_encoding = face_encoding[0]
+            known_face_encodings.append(face_encoding)
+            known_face_names.append(name[0:name.index(".")])
+        else:
+            print("No faces found in the image!")
+        
 
     while True:
 
         ret, frame = video_capture.read()
-
+        if not ret:
+            break
         if process_this_frame:
 
             small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
@@ -55,17 +61,19 @@ for name in os.listdir(path_dir):
             bottom *= 4
             left *= 4
 
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            cv2.rectangle(frame, (left, top), (right, bottom), (255, 0, 0), 2)
 
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (255, 0, 0), cv2.FILLED)
             font = cv2.FONT_HERSHEY_DUPLEX
             cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
         cv2.imshow('frame', frame)
-        key=cv2.waitKey(1)
-        if key==27:
+        key = cv2.waitKey(1)
+        if key == 27:
             break
-video_capture.release()
-cv2.destroyAllWindows()
-
-
+                
+    video_capture.release()
+    cv2.destroyAllWindows()
+except Exception as e:
+    print(e)
+    sys.exit(1)

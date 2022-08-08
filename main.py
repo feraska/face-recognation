fac51e24d43d87be76b0b4a25e2190c1631ef4b2
feraska <45358056+feraska@users.noreply.github.com>
2022-08-08 17:52:44 +0300

    face-recognation

--- main.py
+++ main.py
@@ -1,28 +1,71 @@
+import os
 
+import face_recognition
 import cv2
-try:
-    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    cap = cv2.VideoCapture(0)
+import numpy as np
+
+video_capture = cv2.VideoCapture(0)
+
+
+path_dir = "images/"
+
+known_face_encodings = []
+known_face_names = []
+for name in os.listdir(path_dir):
+    image = face_recognition.load_image_file(path_dir+"/"+name)
+    face_encoding = face_recognition.face_encodings(image)[0]
+    known_face_encodings.append(face_encoding)
+    known_face_names.append(name[0:name.index(".")])
+
+
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    process_this_frame = True
 
     while True:
-        ret, img = cap.read()
 
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.2,
-            minNeighbors=5,
-            minSize=(20, 20)
-        )
-        for (x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            roi_gray = gray[y:y + h, x:x + w]
-            roi_color = img[y:y + h, x:x + w]
-        cv2.imshow('video',img)
-        k = cv2.waitKey(1)
-        if k == 27:
+        ret, frame = video_capture.read()
+
+        if process_this_frame:
+
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+            rgb_small_frame = small_frame[:, :, ::-1]
+
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+            face_names = []
+            for face_encoding in face_encodings:
+
+                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                name = "Unknown"
+                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
+
+                face_names.append(name)
+
+        process_this_frame = not process_this_frame
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        cv2.imshow('frame', frame)
+        key=cv2.waitKey(1)
+        if key==27:
             break
-    cap.release()
-    cv2.destroyAllWindows()
-except Exception as e:
-    print(e)
+video_capture.release()
+cv2.destroyAllWindows()
+
+
